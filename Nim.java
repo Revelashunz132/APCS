deb076067f44c933c40867240c6f6579a7c426f9
Revelashunz <54650086+Revelashunz132@users.noreply.github.com>
2023-01-18 06:00:17 +0000

    part2

--- Nim.java
+++ Nim.java
@@ -12,6 +12,9 @@ public class Nim {
     public Nim(int[] p){
         piles = p;
         sums = new int[4];
+    }
+    
+    public void analyze(){
         for(int i = 0; i < piles.length; i++){
             for(int j = 0; j < 4; j++){
                 sums[j] += (piles[i] >> j) & 1; //the binary number of each index of the piles will be added to the corresponding index of the sums array
@@ -20,12 +23,30 @@ public class Nim {
         
     }
 
+    public int whichPile(){
+        int bit = sums.length -1;
+        while (bit >= 0 && sums[bit] % 2 == 0){
+            bit --;
+        }
+        if (bit < 0){
+            return -1;
+        }
+        for (int i = 0; i < piles.length; i++){
+            if (((piles[i] >> bit) & 1) == 1){
+                return i;
+            }
+        }
+        return 0;
+    }
+
     public int[] getSums(){
         return sums;
     }
 
     public static void main (String args[]){
-        Nim game1 = new Nim(new int[]{7,8,9});
+        Nim game1 = new Nim(new int[]{8,1,9});
+        game1.analyze();
         System.out.println(Arrays.toString(game1.getSums()));
+        System.out.println(game1.whichPile());
     }
 }
